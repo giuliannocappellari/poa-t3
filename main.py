ed6d74ba3270ac85e7e0b4bd99c5589c2503c02e
Giuliano Barbosa <giuliannocappellari@gmail.com>
2024-10-23 23:43:51 -0300

    final version

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ solutions_set = set()
 
 
 def hash_board(board):
-    list_of_tuples = tuple(tuple(inner_list) for inner_list in board)
+    list_of_tuples = hash(tuple(tuple(inner_list) for inner_list in board))
     return list_of_tuples
 
 
@@ -13,8 +13,8 @@ def create_board(n):
     return [["." for _ in range(n)] for _ in range(n)]
 
 
+# Verifica se eu nao vou apontar pra ninguem da minha quadrilha
 def is_safe(board, row, col, quadrilha):
-    n = len(board)
     directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 
     for dr, dc in directions:
@@ -38,8 +38,7 @@ def is_safe(board, row, col, quadrilha):
     return True
 
 
-def sees_at_least_two_enemies(board, row, col, quadrilha):
-    n = len(board)
+def sees_at_least_two_enemies(board, row, col, quadrilha, n):
     directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
     count = 0
     for dr, dc in directions:
@@ -49,28 +48,29 @@ def sees_at_least_two_enemies(board, row, col, quadrilha):
             if cell == ".":
                 r += dr
                 c += dc
+                if count >= 2:
+                    return True
                 continue
             elif cell != quadrilha:
                 count += 1
                 break
             else:
                 break
-            r += dr
-            c += dc
+        if count >= 2:
+            return True
     return count >= 2
 
 
 def validate_board(board):
     if hash_board(board) not in solutions_set:
-        n = len(board)
         for row in range(n):
             for col in range(n):
                 cell = board[row][col]
                 if cell != ".":
-                    if not sees_at_least_two_enemies(board, row, col, cell):
+                    if not sees_at_least_two_enemies(board, row, col, cell, n):
                         return
         solutions_set.add(hash_board(board))
-        transforms(board)
+        transforms(board, n)
 
 
 def print_board(board, solution):
@@ -80,12 +80,11 @@ def print_board(board, solution):
     print("-" * (len(board[0]) * 5))
 
 
-def transforms(board):
-    n = len(board)
+def transforms(board, n):
 
     def rotate(board):
         return [[board[n - j - 1][i] for j in range(n)] for i in range(n)]
-    
+
     def mirror_matrix(board):
         # Mirror each row of the matrix by reversing it
         mirrored_board = [row[::-1] for row in board]
@@ -103,38 +102,38 @@ def transforms(board):
         solutions_set.add(hash_board(current_board))
 
 
-def backtrack(board, b_left, c_left, total_positions, index):
-    if b_left == 0 and c_left == 0:
+def backtrack(board, b_restantes, c_restantes, total_positions, index):
+    if b_restantes == 0 and c_restantes == 0:
         validate_board(board)
         return
 
     if index >= total_positions:
         return
 
-    n = len(board)
     row, col = divmod(index, n)
 
     if board[row][col] == ".":
         # Tenta colocar um 'B'
-        if b_left > 0 and is_safe(board, row, col, "B"):
+        if b_restantes > 0 and is_safe(board, row, col, "B"):
             board[row][col] = "B"
-            backtrack(board, b_left - 1, c_left, total_positions, index + 1)
+            backtrack(board, b_restantes - 1, c_restantes, total_positions, index + 1)
             board[row][col] = "."
 
         # Tenta colocar um 'C'
-        if c_left > 0 and is_safe(board, row, col, "C"):
+        if c_restantes > 0 and is_safe(board, row, col, "C"):
             board[row][col] = "C"
-            backtrack(board, b_left, c_left - 1, total_positions, index + 1)
+            backtrack(board, b_restantes, c_restantes - 1, total_positions, index + 1)
             board[row][col] = "."
 
     # Pular a posição atual
-    backtrack(board, b_left, c_left, total_positions, index + 1)
+    backtrack(board, b_restantes, c_restantes, total_positions, index + 1)
 
 
 def main():
     from time import time
 
     start = time()
+    global n
     n = int(sys.argv[1])
     b = int(sys.argv[2])
     c = int(sys.argv[3])
